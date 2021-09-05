77ebcd43f099addda71999d56566b1dd0a569a00
megamaz <raphael.mazuel@gmail.com>
2021-09-05 16:47:51 -0700

    added CJD and Heck to !latest

--- commands/latest.py
+++ commands/latest.py
@@ -12,11 +12,19 @@ async def command(ctx):
     p = {
         "per_page": 1
     }
-    r = requests.get(f"{baseUrl}/repos/Aeroluna/NoodleExtensions/releases", headers=headers, params=p)
-    data = json.loads(r.text)[0]
+    noodle_extension = requests.get(f"{baseUrl}/repos/Aeroluna/NoodleExtensions/releases", headers=headers, params=p)
+    data_ne = json.loads(noodle_extension.text)[0]
 
-    embed = (discord.Embed(title=data['name'], description=data['body'].replace('#', ''), color=discord.Color.blurple())
-    .set_thumbnail(url=data['author']['avatar_url'])
-    .add_field(name="Download", value=f'[Download {data["name"]}]({data["assets"][0]["browser_download_url"]})'))
+    custom_json_data = requests.get(f"{baseUrl}/repos/Aeroluna/CustomJSONData/releases", headers=headers, params=p)
+    data_cjd = json.loads(custom_json_data.text)[0]
+
+    heck = requests.get(f"{baseUrl}/repos/Aeroluna/Heck/releases", headers=headers, params=p)
+    data_heck = json.loads(heck.text)[0]
+
+    embed = (discord.Embed(title=data_ne['name'], description=data_ne['body'].replace('#', ''), color=discord.Color.blurple())
+    .set_thumbnail(url=data_ne['author']['avatar_url'])
+    .add_field(name="Downloads", value=f'''[Download NE{data_ne["name"]}]({data_ne["assets"][0]["browser_download_url"]})
+[Download CJD{data_cjd["name"]}]({data_cjd["assets"][0]["browser_download_url"]})
+[Download Heck{data_heck["name"]}]({data_heck["assets"][0]["browser_download_url"]})'''))
     
     await ctx.send(embed=embed)
