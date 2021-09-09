a120d4ec0a67540552c09a28be16a9663e47d0e6
megamaz <raphael.mazuel@gmail.com>
2021-09-08 18:02:25 -0700

    improved randid command

--- commands/randID.py
+++ commands/randID.py
@@ -3,14 +3,18 @@ import asyncio
 import requests # check to see if the map exists
 
 async def command(ctx):
-    mess = await ctx.send("Finding random existing map...")
+    attempts = 0
+    mess = await ctx.send(f"Finding random existing map... Attempt {attempts+1}")
     rand = hex(random.randint(1,0xfffff))[2:]
     r = requests.get(f'https://beatsaver.com/api/maps/id/{rand}')
-    while r.status_code == 404:
-        rand = hex(random.randint(1,0xfffff))[2:]
-        r = requests.get(f'https://beatsaver.com/api/maps/id/{rand}')
-        if r.status_code != 404:
-            break
-        await asyncio.sleep(1)
+    if r.status_code == 404:
+        while True:
+            rand = hex(random.randint(1,0xfffff))[2:]
+            r = requests.get(f'https://beatsaver.com/api/maps/id/{rand}')
+            if r.status_code != 404:
+                break
+            attempts += 1
+            await mess.edit(content=f"Finding random existing map... Attempt {attempts+1}")
+            await asyncio.sleep(1)
     
     await mess.edit(content=f"Your random ID is {rand}. https://beatsaver.com/maps/{rand}")
