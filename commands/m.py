0af72490446890f38c1daabeafb51f74e156edec
megamaz <raphael.mazuel@gmail.com>
2022-01-05 16:42:36 -0800

    added macro command

---
+++ commands/m.py
@@ -0,0 +1,14 @@
+# macros
+
+import json
+import discord
+from discord.ext import commands
+
+async def command(ctx:commands.Context, name:str):
+    macro = json.load(open('./macros.json'))
+    if not macro.get(name):
+        await ctx.send("That macro doesn't exist.")
+        return
+    data = macro[name]
+    embed = discord.Embed(title=data['title'], description=data['content'], color=discord.Colour.blurple())
+    await ctx.send(embed=embed)
