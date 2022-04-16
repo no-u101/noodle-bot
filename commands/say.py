c316e7d09417b280df6635854a51cfc1e2eec507
megamaz <raphael.mazuel@gmail.com>
2022-04-15 21:52:24 -0700

    noodle bot is typing

--- commands/say.py
+++ commands/say.py
@@ -6,8 +6,10 @@ async def command(ctx, *args):
     
     if any([y in "administrator staff founder" for y in [x.name.lower() for x in ctx.author.roles]]):
         c = ctx.guild.get_channel(int(args[0]))
-        await c.trigger_typing()
         message = ' '.join(args[1:])
-        await asyncio.sleep(len(message) / 10)
+        time = len(message) / 10
+        for _ in range(int(time/10)):
+            await c.trigger_typing()
+            await asyncio.sleep(10)
         await c.send(message)
     
