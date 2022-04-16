b44f1f606182cce12516c4b83001a829c3002ba6
megamaz <raphael.mazuel@gmail.com>
2022-04-15 21:44:52 -0700

    fixed some message_delet stuff

--- bot_cog.py
+++ bot_cog.py
@@ -38,19 +38,12 @@ class ModLogger(commands.Cog):
                     
     @commands.Cog.listener()
     async def on_message_delete(self, message:discord.Message):
-        if message.channel_id != 890689165442809888:
-            if message.cached_message:
-                embed = discord.Embed(title='Message Deleted', description=f'"{message.cached_message.content}"\nFrom: {message.cached_message.author}\nIn: <#{message.channel_id}>', color=discord.Colour.blurple())
-                if len(message.attachments) != 0:
-                    embed.add_field(name='The following image was attached:', value=f'{message.attachments[0].filename}')
-                    embed.set_image(url=message.attachments[0].url)
-                await self.logs_channel.send(embed=embed)
-            else:
-                embed = discord.Embed(title='Message Deleted', description=f'Message content unknown.\nFrom: unknown.\nIn: <#{message.channel_id}>', color=discord.Colour.blurple())
-                if len(message.attachments) != 0:
-                    embed.add_field(name='Images', value=f'{message.attachments[0].filename}')
-                    embed.set_image(url=message.attachments[0].url)
-                await self.logs_channel.send(embed=embed)
+        if message.channel.id != 890689165442809888:
+            embed = discord.Embed(title='Message Deleted', description=f'"{message.cached_message.content}"\nFrom: {message.cached_message.author}\nIn: <#{message.channel_id}>', color=discord.Colour.blurple())
+            if len(message.attachments) != 0:
+                embed.add_field(name='The following image was attached:', value=f'{message.attachments[0].filename}')
+                embed.set_image(url=message.attachments[0].url)
+            await self.logs_channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member:discord.Member):
